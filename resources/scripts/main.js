13856c47764ad272c828f1c8758ea6fda8572e06
movleax <6619780+movleax@users.noreply.github.com>
2018-12-12 14:47:37 -0800

    Rework index for portfolio. Add some pictures. add a main.js for basic website flow.

---
+++ resources/scripts/main.js
@@ -0,0 +1,34 @@
+$( document ).ready( () => {
+    $('.splashscreen-text').fadeIn(2000, "swing");
+    $(".splashscreen-text").animate({height: '150px', width: '150px'}, 125, () =>{
+        $("#splashscreen-loader").fadeIn(400);
+        setTimeout(() => {
+            AnimateIntro();
+        }, 4000);
+    });
+});
+
+$('.splashscreen').on("mousemove", () =>{
+    AnimateIntro();
+});
+
+function AnimateIntro()
+{
+    HideSplashScreen(1000, 1000);
+    ShowSidePanel(250, 3000);
+}
+
+function HideSplashScreen(animationSpeed, timeout)
+{
+    $("#splashscreen-loader").css("border-left", "7px solid #3498db");
+    $("#splashscreen-loader").css("border-right", "7px solid #3498db");
+    $("#splashscreen-loader").css("border-bottom", "7px solid #3498db");
+
+    setTimeout(() => { $('.splashscreen').fadeOut(animationSpeed); }, timeout);
+}
+
+function ShowSidePanel(animationSpeed, timeout)
+{
+    setTimeout(() => { $('.sidebar').animate({left: '0px'}, animationSpeed); }, timeout);
+}
+
