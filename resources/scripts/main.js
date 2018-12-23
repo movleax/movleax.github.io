9c88148484edef85eacc8a51cad733a4db2123ca
movleax <6619780+movleax@users.noreply.github.com>
2018-12-23 14:55:43 -0800

    finished creating basic homepage. Added homepage css to style.css Created a way to dynamically create the homepage

--- resources/scripts/main.js
+++ resources/scripts/main.js
@@ -1,3 +1,42 @@
+const homeData = [
+    {
+        title: "MazeSolveJS",
+        contentInfo: "MazeSolverJS is a JavaScript, canvas-based web app. This web app is designed to load in a map file, which contains ASCII characters for specifying the type of object to create, and the screen location of said object. For instance, when the map parser reads in a 'X' character, this tells the JavaScript program that a object of type Block should be created and pushed onto an array. Using basic math and knowing which iteration of parsing through the map file, a X and Y position can be derived and assigned to created objects. In the future, the app will have a dropdown list to choose which algorithm the maze should use to solve a given map. Currently, the solver object chooses a random direction once it has hit a wall.",
+        photo: "https://raw.githubusercontent.com/movleax/MazeSolverJS/master/simple_maze_solver_algo.gif",
+        buttons: [["new-tab", "https://github.com/movleax/MazeSolverJS", "GitHub Project"], ["load", "https://movleax.github.io/MazeSolverJS/index.html", "Load"]]
+    },
+    {
+        title: "Map Generator",
+        contentInfo: "Top-down 2D Map generator in C#. The program reads shape files from a designated folder, and performs calculations to connect these shapes in a cohesive manner. ",
+        photo: "https://raw.githubusercontent.com/movleax/dungeonGeneratorPoC/master/readme_resources/dungeon_generator_running.gif",
+        buttons: [["new-tab", "https://github.com/movleax/dungeonGeneratorPoC", "GitHub Project"]]
+    },
+    {
+        title: "StopWatch++",
+        contentInfo: "Created a WPF application to help keep track of how long certain work tasks take. This also helps organize specific tasks and provides good feedback. Utilized the Xceed toolkit to create child windows inside of the main window. Tasks can be created, deleted. Each task has a timer value hh:mm:ss, start and stop button for the timer, and a logging mechanism. Log items for a given task include the title, but also contain an optional description. Log items can be modified once created.",
+        photo: "resources\\images\\stopwatchpp.jpg",
+        buttons: [["new-tab", "https://github.com/movleax/StopWatch", "GitHub Project"]]
+    },
+    {
+        title: "Hipster Delivery",
+        contentInfo: "Created with Unity and C#, this android game plays as a 2D \"Infinite Runner\", where the objective is to deliver papers while avoiding obstacles.",
+        photo: "resources\\images\\hipsterDeliverRun.jpg",
+        buttons: [["new-tab", "https://play.google.com/store/apps/details?id=com.austindolve.hipsterdelivery&hl=en_US", "Google Play Store"]]
+    },
+    {
+        title: "Space Patrol",
+        contentInfo: "Though an old project, space patrol is an exciting 2D side-scrolling shooter where obstacles must avoided and enemies beat. Written in C++ and SDL.",
+        photo: "resources\\images\\space_partol.PNG",
+        buttons: [["load", "", "More Info Coming soon"]]
+    },
+    {
+        title: "Mario Clone",
+        contentInfo: "Another interesting classic written in C++. This offshoot mario clone offers a fun 2D platformer experience where items must be collected and enemies defeated.",
+        photo: "resources\\images\\jacob_sdl.PNG",
+        buttons: [["load", "", "More Info Coming soon"]]
+    }
+];
+
 const projectsData = [
     {
         title: "MazeSolveJS",
@@ -88,6 +127,35 @@ function LoadProjects() {
 
 }
 
+function homepageTemplate(content){
+    return `
+    <div class="content-element">
+        <div>
+            <img class="content-picture" src="${content.photo}" alt="${content.title}">
+        </div>
+        <div class="content-information">
+            <div class="content-title">
+                ${content.title}
+            </div>
+            <div class="content-details">
+                ${content.contentInfo}
+            </div>
+            ${content.buttons ? getButtons(content.buttons) : ''}
+        </div>
+    </div>
+    `;
+}
+
+function LoadHomePage(){
+    $(".container").empty();
+
+    document.getElementsByClassName("container")[0].innerHTML = `
+    <div class='home'>
+    ${homeData.map(homepageTemplate).join('')}
+    </div>
+    `;
+}
+
 function LoadContactInfo()
 {
     $(".container").empty();
@@ -121,10 +189,12 @@ $( document ).ready( () => {
         }, 3000);
     });
 
+    LoadHomePage();
+
     $(".sidebar a").on("click", (e)=>{
         switch(e.target.id)
         {
-            case "home": $(".container").empty(); break;
+            case "home": LoadHomePage(); break;
             case "projects": LoadProjects(); break;
             case "contact": LoadContactInfo(); break;
             case "about": $(".container").empty(); break;
